df0bf706f59be893f51ea77cc038a5aef661e4ff
Higor Granzoto <higorrg@gmail.com>
2025-08-09 18:33:12 -0300

    Cleanup

--- backend/src/main/java/br/com/releasemanager/release/application/port/in/AddClientEnvironmentCommand.java
+++
@@ -1,9 +0,0 @@
-package br.com.releasemanager.release.application.port.in;
-
-import br.com.releasemanager.release.domain.model.Environment;
-
-public record AddClientEnvironmentCommand(
-    Long releaseId,
-    String clientCode,
-    Environment environment
-) {}
